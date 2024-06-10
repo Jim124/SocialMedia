6fb2a26081f47d49aa13b2a1320d50bea934a1c6
Jim124 <fengpingan065@gmail.com>
2024-06-10 12:20:20 +0800

    add expo-font to project

--- App.js
+++ App.js
@@ -1,5 +1,39 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
 
+import * as SplashScreen from 'expo-splash-screen';
+import { getFontFamily } from './assets/fonts/helper';
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
-  return <SafeAreaView></SafeAreaView>;
+  const [fontsLoaded, fontError] = useFonts({
+    'inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+    'inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+    'inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.ttf'),
+    'inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'inter-Thin': require('./assets/fonts/Inter-Thin.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  return (
+    <SafeAreaView onLayout={onLayoutRootView}>
+      <Text style={styles.text}>Hello,World</Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  text: { fontFamily: getFontFamily('inter', '300'), fontSize: 50 },
+});
